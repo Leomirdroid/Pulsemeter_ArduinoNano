f96ab0978fdcef7519b175694b51e5cadecab942
Leo Pöttinger <lpoettinger@thorlabs.com>
2023-07-10 17:58:11 +0200

    Auswerung_Steigung

--- Pulsemeter_ArduinoNano/Pulsemeter_ArduinoNano.ino
+++ Pulsemeter_ArduinoNano/Pulsemeter_ArduinoNano.ino
@@ -2,6 +2,13 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_TSL2561_U.h>
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
+const int l = 200;
+int data[l];
+int dataeval[l-1];
+int dataeval2[l-10];
+int peaks;
+sensors_event_t event;
+
 void setup() {
    Serial.begin(9600);
   Serial.println("Light Sensor Test"); Serial.println("");
@@ -19,25 +26,54 @@ void setup() {
   // put your setup code here, to run once:
 tsl.setGain(TSL2561_GAIN_16X);
 tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_13MS);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 /* Get a new sensor event */ 
-  sensors_event_t event;
-  tsl.getEvent(&event);
  
-  /* Display the results (light is measured in lux) */
-  if (event.light)
-  {
-    Serial.println(event.light);
-  }
-  else
-  {
-    /* If event.light = 0 lux the sensor is probably saturated
-       and no reliable data could be generated! */
-    Serial.println("Sensor overload");
-  }
-  delay(20);
   
+  for (int i = 0; i < l ; i++){
+  tsl.getEvent(&event);
+   data[i] = event.light;
+   delay(7);
+  } 
+   
+  /* Display the results (light is measured in lux) */
+for (int i = 0; i < l ; i++){
+    //Serial.println(data[i]);
+  }
+
+for (int i = 0; i < l-1 ; i++){
+  
+  if (data[i+1] >= data[i]) {
+    dataeval[i] = 1;
+  }
+    else {
+      dataeval[i] = 0;
+    }
+ for (int i = 0; i < l-10 ; i++){
+  
+  if ((dataeval[i]+dataeval[i+1]+dataeval[i+2]+dataeval[i+3]+dataeval[i+4]+dataeval[i+5]+dataeval[i+6]+dataeval[i+7]+dataeval[i+8]+dataeval[i+9]) > 9) {
+    dataeval2[i] = 1;
+  }
+    else {
+      dataeval2[i] = 0;
+    } 
+ }  
+ /*for (int i = 0; i < l ; i++){
+    for (int j = 0; j < 15; j++)
+    if (dataeval[j]
+    peaks = peaks + 1;
+  }    */
+}
+  for (int i = 0; i < l-10 ; i++){
+    Serial.print(data[i]);
+    Serial.print(" ");
+    Serial.print(dataeval[i]*300);
+        Serial.print(" ");
+    Serial.println(dataeval2[i]*300);
+  }
+
 }
